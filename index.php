279b29657f1463babc9e294d6e42093cea600c47
batuhansanat <batuhan.sanat@gmail.com>
2022-11-06 19:56:34 +0300

    finished

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-
+function ucgen($adet){
+    for($i=0; $i <= $adet; $i++){
+        $j = 0;
+        while($j < $i){
+            echo "0";
+            $j++;
+        }
+        echo "<br>";
+    }
+}
+ucgen(15)
 
 ?>
